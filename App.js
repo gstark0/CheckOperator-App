353f443bbc95d83fda4274893fbfbe71a671f725
gstark0 <gstark0@icloud.com>
2019-04-09 12:43:53 +0200

    Navigation

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
 import React from 'react';
+import ContactsList from './components/ContactsList';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const AppStackNavigator = createStackNavigator({
+	ContactsList: {
+		screen: ContactsList
+	}
 });
+
+const App = createAppContainer(AppStackNavigator);
+export default App;
---
+++ components/ContactsList/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, View, Text, Button, Image, Linking, TouchableOpacity } from 'react-native';
+
+
+class ContactsList extends React.Component {
+	render() {
+		return (
+			<View>
+				<Text>Kontakty</Text>
+			</View>
+		);
+	}
+}
+
+export default ContactsList;
